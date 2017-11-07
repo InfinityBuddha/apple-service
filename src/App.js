e782a5dc1ba74792ace20714dea9d3bdf199e618
Руслан Королев <techfusionwork@gmail.com>
2017-11-07 21:00:09 +0300

    Добавлен footer для ipad версии

--- src/App.js
+++ src/App.js
@@ -231,9 +231,44 @@ class App extends Component {
 
                 <footer className={f.footer}>
                     <div className={f.container}>
-                        <a href="#" className={f.title}>ApplePremiumService</a>
-                        <div className={f.phones}>+7(985) 070-80-80 <br /> +7(916) 611-55-50</div>
-                        <a href="https://vk.com" className={f.vk}></a>
+                        <div className={f.footerInfo}>
+                            <div className={f.logo}>
+                                <a href="#" className={f.title}>ApplePremiumService</a>
+                            </div>
+                            <div className={f.repair}>
+                                <h3 className={f.title}>Ремонт</h3>
+                                <ul className={f.list}>
+                                    <li>Ремонт iPhone</li>
+                                    <li>Ремонт iPad</li>
+                                    <li>Ремонт MacBook</li>
+                                    <li>Ремонт iMac</li>
+                                    <li>Ремонт Samsung</li>
+                                    <li>Ремонт Других устройств</li>
+                                </ul>
+                            </div>
+                            <div className={f.sale}>
+                                <h3 className={f.title}>Продажа</h3>
+                                <ul className={f.list}>
+                                    <li>iPhone</li>
+                                    <li>Дисплеи</li>
+                                </ul>
+                            </div>
+                            <div className={f.phones}>
+                                <p className={f.phone}>+7(985) 070-80-80</p>
+                                <h3 className={f.title}>Адреса сервис-центров</h3>
+                                <ul className={f.list}>
+                                    <li>г.Руза</li>
+                                    <li>+7(985) 070-80-80</li>
+                                    <li >г. Можайск</li>
+                                    <li>+7(916) 611-55-50</li>
+                                    <li>График работы:</li>
+                                    <li>ПН-ПТ 10:00 - 18:00</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={f.footerSocial}>
+                            <a className={f.vk} href="https://vk.com" ></a>
+                        </div>
                     </div>
                 </footer>
             </div>
